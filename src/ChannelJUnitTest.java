59347f0f07a48b4c92caad072099c1db172aa0ec
bsheps <bshepard@uwm.edu>
2018-05-08 20:02:14 -0500

    Finished channelJunit

--- src/ChannelJUnitTest.java
+++ src/ChannelJUnitTest.java
@@ -11,7 +11,7 @@ class ChannelJUnitTest {
 		}
 	}
 	@Test
-	void testToggle() {
+	void testToggleAndisEnabled() {
 		c = new Channel();
 		for(int i =0; i<13; i++) {
 			assertTrue(c.Toggle(i));
@@ -23,5 +23,28 @@ class ChannelJUnitTest {
 		}
 		assertFalse(c.Toggle(-1));
 		assertFalse(c.Toggle(13));
+	}
+	@Test
+	void testconnectSensoranddisconnectSensor() {
+		c = new Channel();
+		for(int i =0; i<13; i++) {
+			assertTrue(c.connectSensor("gate",i));		
+		}
+		for(int i =0; i<13; i++) {
+			assertEquals(c.getSensor(i),"gate");		
+		}
+		for(int i =0; i<13; i++) {
+			assertTrue(c.disconnectSensor(i));		
+		}
+		for(int i =0; i<13; i++) {
+			assertEquals(c.getSensor(i),null);		
+		}
+		assertFalse(c.connectSensor("", -1));
+		assertFalse(c.connectSensor("", 15));
+		assertFalse(c.connectSensor(null, 3));
+		assertFalse(c.disconnectSensor(-1));
+		assertFalse(c.disconnectSensor(14));
+		assertTrue(c.connectSensor("gate", 1));
+		assertFalse(c.connectSensor("gate", 1));
 	}
 }
