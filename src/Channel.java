2fc95b426287289417a61943f470b795ec0d5a1e
bjf <bjf@10.193.201.127>
2018-05-08 19:02:10 -0500

    updated channel & test cases to check boundary values

--- src/Channel.java
+++ src/Channel.java
@@ -2,29 +2,51 @@
 public class Channel {
 	private boolean[] channel = new boolean[13]; // 0 slot left empty
 	private String[] sensor = new String[13]; // 0 slot left empty
+
 	public Channel() {
-		for(int i = 0; i < channel.length; i++) 
+		for (int i = 0; i < channel.length; i++)
 			channel[i] = false;
 	}
+
 	public boolean Toggle(int ch) {
-		channel[ch] = !channel[ch];
-		return channel[ch];
+		if (ch > 0 && ch < channel.length) {
+			channel[ch] = !channel[ch];
+			return channel[ch];
+		}
+		return false;
 	}
+
 	public boolean isChannelEnabled(int channelNumber) {
-		return channel[channelNumber];
+		if (channelNumber > 0 && channelNumber < channel.length) {
+			return channel[channelNumber];
+		}
+		return false;
 	}
+
 	public boolean connectSensor(String sensortype, int chan) {
-		if(sensor[chan] != null) {return false;/*can't connect because something is already there*/}
-		else {
-			sensor[chan] = sensortype;
-			return true;
+		if (sensortype == null) {
+			return false;
 		}
+		if (chan > 0 && chan < channel.length) {
+			if (sensor[chan] != null) {
+				return false;
+				/* can't connect because something is already there */} else {
+				sensor[chan] = sensortype;
+				return true;
+			}
+		}
+		return false;
 	}
+
 	public boolean disconnectSensor(int chan) {
-		if(sensor[chan] == null) return false;
-		else {
-			sensor[chan]= null;
-			return true;
+		if (chan > 0 && chan < channel.length) {
+			if (sensor[chan] == null)
+				return false;
+			else {
+				sensor[chan] = null;
+				return true;
+			}
 		}
+		return false;
 	}
 }
---
+++ src/TestChannel.java
@@ -0,0 +1,61 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestChannel {
+
+	@Test
+	void testChannel() {
+		Channel c1 = new Channel();
+
+		// test boundary cases for channels
+		for (int i = -1; i < 14; i++) {
+			assertFalse(c1.isChannelEnabled(i));
+		}
+
+		// test boundary cases for toggling
+		assertFalse(c1.Toggle(-1));
+		assertFalse(c1.Toggle(0));
+		for (int i = 1; i < 13; i++) {
+			assertTrue(c1.Toggle(i));
+		}
+		assertFalse(c1.Toggle(13));
+		assertFalse(c1.Toggle(14));
+
+		// test boundary cases for disconnecting sensors
+		assertFalse(c1.disconnectSensor(-1));
+		assertFalse(c1.disconnectSensor(0));
+		for (int i = 1; i < 13; i++) {
+			assertFalse(c1.disconnectSensor(i));
+		}
+		assertFalse(c1.disconnectSensor(13));
+		assertFalse(c1.disconnectSensor(14));
+
+		String sensor = "GATE";
+
+		// test boundary cases for connection sensors
+		assertFalse(c1.connectSensor(sensor, -1));
+		assertFalse(c1.connectSensor(sensor, 0));
+		for (int i = 1; i < 13; i++) {
+			assertTrue(c1.connectSensor(sensor, i));
+		}
+		assertFalse(c1.connectSensor(sensor, 13));
+		assertFalse(c1.connectSensor(sensor, 14));
+
+		// try to insert sensors where there are already sensors
+		for (int i = 1; i < 13; i++) {
+			assertFalse(c1.connectSensor(sensor, i));
+		}
+
+		// disconnect sensors
+		for (int i = 1; i < 13; i++) {
+			assertTrue(c1.disconnectSensor(i));
+		}
+
+		for (int i = 1; i < 13; i++) {
+			assertFalse(c1.connectSensor(null, i));
+		}
+	}
+
+}
