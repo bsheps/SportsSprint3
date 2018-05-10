44a00afa4e86c0de2595196a72727887883df1ea
bshepard <bshepard@ceas-e270-07.ad.uwm.edu>
2018-05-10 15:37:29 -0500

    Adjusted to work with older version of junit

--- src/IndividualEventJUnitTest.java
+++ src/IndividualEventJUnitTest.java
@@ -1,8 +1,8 @@
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Queue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 class IndividualEventJUnitTest {
 	IndividualEvent n;
